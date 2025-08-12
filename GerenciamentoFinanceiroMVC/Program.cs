f9e3aff328acb648031abb058e59eb384e6e4fe2
Pedro Laia <ti.pedrohls@outlook.com>
2025-08-12 18:58:36 -0300

    #001 - Implementação de Conexão/Criação de DB

--- GerenciamentoFinanceiroMVC/Program.cs
+++ GerenciamentoFinanceiroMVC/Program.cs
@@ -1,7 +1,18 @@
+using GerenciamentoFinanceiroMVC.Data;
+using Microsoft.AspNetCore.Connections;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
+builder.Services.AddDbContext<AppDbContext>
+(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+}
+);
 
 var app = builder.Build();
 
