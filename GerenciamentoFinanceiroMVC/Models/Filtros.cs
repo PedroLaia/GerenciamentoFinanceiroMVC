ddabfb7bad98919ba4011d0550a376e2f985de48
Pedro Laia <ti.pedrohls@outlook.com>
2025-08-13 16:45:27 -0300

    #002 - Estruturação, Implementação de Template CSS e Criação de View Filtros

---
+++ GerenciamentoFinanceiroMVC/Models/Filtros.cs
@@ -0,0 +1,40 @@
+﻿namespace GerenciamentoFinanceiroMVC.Models
+{
+    public class Filtros
+    {
+        public string FiltroString { get; set; }
+        public string CategoriaId { get; set; }
+        public string TransacaoId { get; set; }
+        public string DataOperacao { get; set; }
+
+        public Filtros(string filtroString)
+        {
+            FiltroString = filtroString ?? "todos-todos-todos";
+            string[] filtros = FiltroString.Split('-');
+
+            CategoriaId = filtros[0];
+            TransacaoId = filtros[1];
+            DataOperacao = filtros[2];
+        }
+        public bool TemCategoria => CategoriaId.ToLower() != "todos";
+        public bool TemTransacao => TransacaoId.ToLower() != "todos";
+        public bool TemDataOperacao => DataOperacao.ToLower() != "todos";
+
+        
+        public static Dictionary<string, string> ValoresDataOperacao =>
+            new Dictionary<string, string>
+            {
+                {"passado", " Passado"},
+                {"futuro", " Futuro" },
+                {"hoje", " Hoje" },
+            };
+        public bool EPassado => DataOperacao.ToLower() == "passado";
+        public bool EFuturo => DataOperacao.ToLower() == "futuro";
+        public bool EHoje => DataOperacao.ToLower() == "hoje";
+
+
+
+
+
+    }
+}
