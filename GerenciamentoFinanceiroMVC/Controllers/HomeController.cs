2952535b982c6acf91438cd8ae22c510b1b94534
Pedro Laia <ti.pedrohls@outlook.com>
2025-08-15 08:36:00 -0300

    #004 - Implementação de Filtros de Consulta DB na Home Controler

--- GerenciamentoFinanceiroMVC/Controllers/HomeController.cs
+++ GerenciamentoFinanceiroMVC/Controllers/HomeController.cs
@@ -1,16 +1,70 @@
 using System.Diagnostics;
+using GerenciamentoFinanceiroMVC.Data;
 using GerenciamentoFinanceiroMVC.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace GerenciamentoFinanceiroMVC.Controllers
 {
     public class HomeController : Controller
     {
-
-        public IActionResult Index()
+        private readonly AppDbContext _context;
+        public HomeController(AppDbContext context)
         {
-            return View();
+            _context = context;
         }
 
+
+
+        public IActionResult Index(string id)
+        {
+
+            var filtros = new Filtros(id);
+
+            ViewBag.Filtros = filtros;
+            ViewBag.Categorias = _context.Categorias.ToList();
+            ViewBag.Transacoes = _context.Transacoes.ToList();
+
+            IQueryable<Financeiro> consulta = _context.Financas
+                .Include(x => x.Transacao).Include(x => x.Categoria);
+
+
+            if (filtros.TemCategoria)
+            {
+                consulta = consulta.Where(c => c.CategoriaId == filtros.CategoriaId);
+            }
+
+            if (filtros.TemTransacao)
+            {
+                consulta = consulta.Where(c => c.TransacaoId == filtros.TransacaoId);
+            }
+
+            if (filtros.TemDataOperacao)
+            {
+                DateTime hoje = DateTime.Today;
+
+                if (filtros.EPassado)
+                {
+                    consulta = consulta.Where(c => c.DataOperacao < hoje);
+                }
+                else if (filtros.EFuturo)
+                {
+                    consulta = consulta.Where(c => c.DataOperacao > hoje);
+                }
+                else
+                {
+                    consulta = consulta.Where(c => c.DataOperacao == hoje);
+                }
+
+            }
+
+            var financas = consulta.OrderBy(d => d.DataOperacao).ToList();
+
+
+                return View();
+            
+
+        }
     }
 }
+
